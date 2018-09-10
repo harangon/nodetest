cd0ed61be31452c89722ebb5e99039e657b61384
Darran <darran@vrani.co.kr>
2018-09-10 17:05:44 +0900

    no message

--- nodetest/antibug/index.js
+++ nodetest/antibug/index.js
@@ -46,44 +46,42 @@ class CEKRequest {
     const intent = this.request.intent.name
     const slots = this.request.intent.slots
 
-    switch (intent) {
-      case 'BugInfoIntent':
-        console.log('BugInfoIntent');
+    if (intent === 'BugInfoIntent') {
+      console.log('BugInfoIntent');
 
-        var bugName = null;
-        if (slots !== null) {
-          const bugSlot = slots.bug;
-          if (bugSlot !== null)
-            bugName = bugSlot.value;
-        }
+      var bugName = null;
+      if (slots !== null) {
+        const bugSlot = slots.bug;
+        if (bugSlot !== null)
+          bugName = bugSlot.value;
+      }
+      
+      if (bugName === null) {
+        console.log('Bug is null!!');
         
-        if (bugName === null) {
-          console.log('Bug is null!!');
-          
-          cekResponse.setSimpleSpeechText("죄송해요. 해충의 이름을 다시 말씀해주세요.");
-          cekResponse.setMultiturn({
-            intent: 'BugInfoIntent',
-          })
-        }
-        else {
-          console.log('Bug: ' + bugName);
+        cekResponse.setSimpleSpeechText("죄송해요. 해충의 이름을 다시 말씀해주세요.");
+        cekResponse.setMultiturn({
+          intent: 'BugInfoIntent',
+        });
+      }
+      else {
+        console.log('Bug: ' + bugName);
 
-          if (bugName === '모기')
-            cekResponse.setSimpleSpeechText(`유칼립투스 오일을 물에 섞어 공기 중에 뿌리면 천연 모기 퇴치제가 되요.`);
-          else if (bugName === '초파리')
-            cekResponse.setSimpleSpeechText(`하수구나 창문에 토마토 즙을 놓아봐요.`);
-          else if (bugName === '바퀴벌레')
-            cekResponse.setSimpleSpeechText(`에탄올과 잘말린 계피를 8대 2 비율로 섞으면 천연 모기퇴치제가 되요.`);
-          else if (bugName === '개미')
-            cekResponse.setSimpleSpeechText(`물린 부위에 우유를 살짝 발라주면 가려움을 줄여줘요.`);
-          else
-            cekResponse.setSimpleSpeechText("죄송해요.", bugName, "에 대해서는 알지 못합니다.");
-        }
-        break;
-
-      case 'Clova.GuideIntent':
-      default:
-        cekResponse.setSimpleSpeechText("죄송해요. 해충의 이름을 말씀해주세요.")
+        if (bugName === '모기')
+          cekResponse.setSimpleSpeechText(`유칼립투스 오일을 물에 섞어 공기 중에 뿌리면 천연 모기 퇴치제가 되요.`);
+        else if (bugName === '초파리')
+          cekResponse.setSimpleSpeechText(`하수구나 창문에 토마토 즙을 놓아봐요.`);
+        else if (bugName === '바퀴벌레')
+          cekResponse.setSimpleSpeechText(`에탄올과 잘말린 계피를 8대 2 비율로 섞으면 천연 모기퇴치제가 되요.`);
+        else if (bugName === '개미')
+          cekResponse.setSimpleSpeechText(`물린 부위에 우유를 살짝 발라주면 가려움을 줄여줘요.`);
+        else
+          cekResponse.setSimpleSpeechText("죄송해요.", bugName, "에 대해서는 알지 못합니다.");
+      }
+    }
+    else
+    {
+      cekResponse.setSimpleSpeechText("죄송해요. 해충의 이름을 말씀해주세요.");
     }
 
     if (this.session.new == false) {
