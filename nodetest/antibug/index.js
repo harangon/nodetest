afee11e9bb520f0bf76d70e5cec3b5145d8b885a
Darran <darran@vrani.co.kr>
2018-09-19 10:38:50 +0900

    no message

--- nodetest/antibug/index.js
+++ nodetest/antibug/index.js
@@ -203,6 +203,7 @@ class CEKRequest {
           cekResponse.setSimpleSpeechText("죄송해요." + bugName + "에 대해서는 알지 못합니다.");
         }
         else {
+          console.log("Msg: " + bugIndex + " : " + messageIndex);
           cekResponse.setSimpleSpeechText(repelMessages[bugIndex][messageIndex]);
           cekResponse.appendSpeechText(bugName + '에게 입은 피해 관리 방법을 알고 싶다면, 벌레헐떡에게 ' + bugName + '에 물렸을 땐 어떡해? 라고 물어보세요. 다른 해충에 대해 알고 싶으면 해충의 이름을 불러주세요.');
         }  
